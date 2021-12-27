f5a0f6a26cba9013a3207ea74fd5fb04e7941e4c
Minh Chien DUONG <mchien.duong@gmail.com>
2021-12-27 21:35:56 +0100

    finished three basic components

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,15 +1,48 @@
+import { Tutorial } from './../../../../.history/src/app/models/tutorial.model_20211227154429';
+import { TutorialService } from 'src/app/services/tutorial.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-tutorial',
   templateUrl: './add-tutorial.component.html',
-  styleUrls: ['./add-tutorial.component.css']
+  styleUrls: ['./add-tutorial.component.css'],
 })
 export class AddTutorialComponent implements OnInit {
+  tutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false,
+  };
 
-  constructor() { }
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private tutorialService: TutorialService) {}
+
+  ngOnInit(): void {}
+
+  saveTutorial(): void {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description,
+    };
+
+    this.tutorialService.create(data).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.submitted = true;
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
   }
 
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false,
+    };
+  }
 }
